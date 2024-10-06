af7d874886a78b4b615227c0cbf4f1c9e7b83d65
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-10-06 14:45:38 +0330

    chore: remove unused function

--- service/src/app.py
+++ service/src/app.py
@@ -2,10 +2,3 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-# TODO: implement the needed endpoints, interacting as needed with the database
-# NOTE: this file imports FastAPI, but you can use Flask if you prefer
-def get_full_name(first_name:str, last_name:str):
-  full_name = first_name.title() + " " + last_name.title()
-  return full_name
-  
