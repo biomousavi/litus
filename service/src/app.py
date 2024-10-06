cd9ce9a735d984ce0c048f697f0fd35ab7d4a9ad
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-10-06 15:19:46 +0330

    feat: add CORS

--- service/src/app.py
+++ service/src/app.py
@@ -1,4 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+# TODO: add origins for deployment
+origins = ["*"]
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app = FastAPI()
 
