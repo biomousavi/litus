a2ed04ae82dde93fd73bd906749fdf82968bae87
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-10-06 15:41:52 +0330

    feat: add model

---
+++ service/src/todo/model.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+class Todo(BaseModel):
+    """
+    Container for a single todo record.
+    """
+
+    # The primary key for the StudentModel, stored as a `str` on the instance.
+    # This will be aliased to `_id` when sent to MongoDB,
+    # but provided as `id` in the API requests and responses.
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
+    title: str
+    creation_time: datetime
+    estimated_time: int
