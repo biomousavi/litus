3aff06255680dde744fcfa22519cc54596135c15
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-10-07 07:33:56 +0330

    feat: add todo model

--- service/src/todo/router.py
+++ service/src/todo/router.py
@@ -1,10 +1,25 @@
 import asyncio
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
-
+# init FastAPI router instance
 router = APIRouter()
 
 @router.get("/todos",
 response_description="List all Todos",)
+async def list_todos():
+    return {"pong": True}
+
+
+@router.post("/todos",
+response_description="Create a Todo",
+status_code=status.HTTP_201_CREATED,
+
+)
+async def list_todos():
+    return {"pong": True}
+
+
+@router.delete("/todos/{id}",
+response_description="Delete a Todo",)
 async def list_todos():
     return {"pong": True}
