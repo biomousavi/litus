d84ca35ea106b9986d91528f84ed3e50c50b2b76
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-10-10 22:21:24 +0330

    build: add mongodb config

--- service/src/app.module.ts
+++ service/src/app.module.ts
@@ -1,11 +1,28 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TodoModule } from './todo/todo.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TodoModule } from "./todo/todo.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { EnvironmentVariables } from "./common/env.validation";
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    TodoModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<EnvironmentVariables>) => ({
+        dbName: configService.get("MONGO_DATEBASE"),
+        uri: `mongodb://${configService.get("MONGO_HOST")}:${configService.get("MONGO_PORT")}`,
+        auth: {
+          username: configService.get("MONGO_USERNAME"),
+          password: configService.get("MONGO_PASSWORD"),
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
