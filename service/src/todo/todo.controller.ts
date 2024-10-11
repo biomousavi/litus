cd495b48901490845d4dbc91b64020632259bbcf
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-10-11 11:34:40 +0330

    feat: update

--- service/src/todo/todo.controller.ts
+++ service/src/todo/todo.controller.ts
@@ -1,34 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { TodoService } from './todo.service';
-import { CreateTodoDto } from './dto/create-todo.dto';
-import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus, HttpCode } from "@nestjs/common";
+import { TodoService } from "./todo.service";
+import { CreateTodoDto } from "./dto/create-todo.dto";
+import { UpdateTodoDto } from "./dto/update-todo.dto";
 
-@Controller('todo')
+@Controller("/todo")
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
-  @Post()
+  @Post("/")
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() createTodoDto: CreateTodoDto) {
     return this.todoService.create(createTodoDto);
   }
 
-  @Get()
+  @Get("/")
   findAll() {
     return this.todoService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.todoService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateTodoDto: UpdateTodoDto) {
     return this.todoService.update(+id, updateTodoDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.todoService.remove(+id);
   }
 }
--- service/src/todo/todo.service.ts
+++ service/src/todo/todo.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException } from "@nestjs/common";
 import { CreateTodoDto } from "./dto/create-todo.dto";
 import { UpdateTodoDto } from "./dto/update-todo.dto";
 import { InjectModel } from "@nestjs/mongoose";
@@ -10,14 +10,18 @@ export class TodoService {
   constructor(@InjectModel(Todo.name) private todoModel: Model<Todo>) {}
 
   async create({ estimated_time, title }: CreateTodoDto): Promise<Todo> {
-    // create new Todo
-    const createdTodo = new this.todoModel({
-      title,
-      estimated_time,
-      creation_time: new Date(),
-    });
-
-    return createdTodo.save();
+    // catching unexpected error and throwing an expected error
+    try {
+      // create new Todo
+      const createdTodo = new this.todoModel({
+        title,
+        estimated_time,
+        creation_time: new Date(),
+      });
+      return createdTodo.save();
+    } catch (error) {
+      throw new InternalServerErrorException();
+    }
   }
 
   findAll() {
