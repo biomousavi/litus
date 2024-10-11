7e27ae3e3459a7464875d0655bd0c865f3596cbf
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-10-11 11:20:07 +0330

    feat: update todo dto

--- service/src/todo/dto/create-todo.dto.ts
+++ service/src/todo/dto/create-todo.dto.ts
@@ -1,9 +1,10 @@
-import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString, Min } from "class-validator";
 export class CreateTodoDto {
   @IsString()
   @IsNotEmpty()
   title: string;
 
   @IsNumber()
+  @Min(0)
   estimated_time: number;
 }
