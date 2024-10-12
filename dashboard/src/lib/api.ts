537197e561663c05ebc645160fa83a2c73e39c7b
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-10-12 18:30:18 +0330

    feat: add type

--- dashboard/src/lib/api.ts
+++ dashboard/src/lib/api.ts
@@ -1,18 +1,23 @@
 import getConfig from "next/config";
+import { RequestInit } from "next/dist/server/web/spec-extension/request";
 
 const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
+
+// Get URL base on SSR or CSR
 const API_URI = serverRuntimeConfig.URI || publicRuntimeConfig.URI;
 
-type Tag = "TODO_LIST" | "GET_TODO"; // etc..
+export type ApiTag = "TODO_LIST" | "GET_TODO"; // etc..
 
-interface RequestInit {
+interface IRequestInit {
   next?: {
-    tags?: Tag[] | undefined | string[];
+    tags?: ApiTag[] | undefined | string[];
   };
 }
 
-export default async function api(url: string, options?: RequestInit) {
+export default async function api(
+  url: string,
+  options?: RequestInit & IRequestInit
+) {
   const newUrl = new URL(url, API_URI);
-
   return fetch(newUrl, options);
 }
