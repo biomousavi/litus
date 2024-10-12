4258a086e157efb4f13b94ac79866b39d2b2a414
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-10-12 12:47:17 +0330

    feat: add server and client side env for next

--- dashboard/next.config.mjs
+++ dashboard/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone'
+  output: 'standalone',
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    URI: 'http://service:4000'
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    URI: 'http://localhost:4000'
+  }
 };
 
 export default nextConfig;
