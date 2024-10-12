6d735bc7dcfc91e0f12d5459b5d234269f73646f
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-10-12 18:09:01 +0330

    feat: add todo list component

---
+++ dashboard/src/components/TodoList/index.tsx
@@ -0,0 +1,16 @@
+import api from "@/lib/api";
+import { TODO } from "@/lib/types";
+import TodoItem from "../TodoItem";
+
+export default async function TodoList() {
+  const response = await api('/todo', { next: { tags: ['TODO_LIST'] } })
+  const todoList: TODO[] = await response.json()
+
+  return (
+    <div className="w-full h-full">
+      <ul className="flex flex-col py-6 items-center gap-y-4 overflow-y-auto w-full h-full px-4 max-h-[60vh]">
+        {todoList.map((item) => <TodoItem key={item._id} todo={item} />)}
+      </ul>
+    </div>
+  );
+}
